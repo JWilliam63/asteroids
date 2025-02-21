d16d34128356a50ff7b70f67511200241f68eb47
Thomo <john.thomo96@gmail.com>
2025-02-21 16:51:58 +0300

    Player added

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pygame
 from constants import *
-
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
     dt = 0
-
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         # limit the framerate to 60 FPS
